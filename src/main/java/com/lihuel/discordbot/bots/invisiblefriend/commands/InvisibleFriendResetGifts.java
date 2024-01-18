80ea2c5fd76a6d665c2a014614d3202c3e366be4
LihuelMujica <lihuelmujica@gmail.com>
2024-01-18 16:38:39 -0300

    add: reset gifts command

---
+++ src/main/java/com/lihuel/discordbot/bots/invisiblefriend/commands/InvisibleFriendResetGifts.java
@@ -0,0 +1,40 @@
+package com.lihuel.discordbot.bots.invisiblefriend.commands;
+
+import com.lihuel.discordbot.annotations.SlashCommand;
+import com.lihuel.discordbot.bots.invisiblefriend.service.InvisibleFriendGameService;
+import com.lihuel.discordbot.discord.commands.Command;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.List;
+
+@SlashCommand({"invisible-friend"})
+
+public class InvisibleFriendResetGifts implements Command {
+    private final InvisibleFriendGameService invisibleFriendGameService;
+
+    public InvisibleFriendResetGifts(InvisibleFriendGameService invisibleFriendGameService) {
+        this.invisibleFriendGameService = invisibleFriendGameService;
+    }
+
+    @Override
+    public String getName() {
+        return "amigo-invisible-reiniciar-regalos";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Reinicia los regalos (borra la base de datos)";
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return null;
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) throws Exception {
+        invisibleFriendGameService.resetGifts(event.getGuild().getId());
+        event.reply("Regalos reiniciados").queue();
+    }
+}
